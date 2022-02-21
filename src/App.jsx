178ff1b638c46ed8fd669de877b3ad5eccb5126f
MTG2000 <mtg0987654321@gmail.com>
2022-02-21 21:28:24 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
 
+var counter = 0;
+
 function App() {
-  return <div></div>;
+  function handleReset() {
+    alert("Reset CLICKED");
+  }
+
+  function handleDecrement() {
+    alert("Decrement CLICKED");
+  }
+
+  function handleIncrement() {
+    counter += 1;
+    console.log(counter);
+  }
+
+  return (
+    <div className="container">
+      <h2 className="value">{counter}</h2>
+
+      <div className="btns">
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={handleIncrement}>Increment</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
